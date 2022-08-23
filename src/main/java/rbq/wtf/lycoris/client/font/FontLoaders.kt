fa98d683fe6eb65992b12b22b93beb6972674e4e
ChaosAttractor <46527539+LostAttractor@users.noreply.github.com>
2022-08-23 19:20:56 +0800

    优化CFont

--- src/main/java/rbq/wtf/lycoris/client/font/FontLoaders.kt
+++ src/main/java/rbq/wtf/lycoris/client/font/FontLoaders.kt
@@ -3,10 +3,10 @@ package rbq.wtf.lycoris.client.font
 import java.awt.Font
 
 object FontLoaders {
-    var default15 = FontRenderer(Font("default", Font.PLAIN, 15), true, true, true)
-    var default16 = FontRenderer(Font("default", Font.PLAIN, 16), true, true, true)
-    var default18 = FontRenderer(Font("default", Font.PLAIN, 18), true, true, true)
-    var default20 = FontRenderer(Font("default", Font.PLAIN, 20), true, true, true)
-    var default25 = FontRenderer(Font("default", Font.PLAIN, 25), true, true, true)
-    var default30 = FontRenderer(Font("default", Font.PLAIN, 30), true, true, true)
+    var default15 = FontRenderer(Font("default", Font.PLAIN, 15), true, true, false)
+    var default16 = FontRenderer(Font("default", Font.PLAIN, 16), true, true, false)
+    var default18 = FontRenderer(Font("default", Font.PLAIN, 18), true, true, false)
+    var default20 = FontRenderer(Font("default", Font.PLAIN, 20), true, true, false)
+    var default25 = FontRenderer(Font("default", Font.PLAIN, 25), true, true, false)
+    var default30 = FontRenderer(Font("default", Font.PLAIN, 30), true, true, false)
 }
